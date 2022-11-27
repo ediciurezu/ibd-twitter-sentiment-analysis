16868fff2ad80620ddeb9a813f96669be4aa7bc4
Eduard Ciurezu <52796726+ediciurezu@users.noreply.github.com>
2022-11-27 22:07:18 +0200

    add preprocessing clean-up of dataset
    Accuracy still bad.
    [TODO] Further work: Try another classifier.

--- src/ml_exploratory_analysis.py
+++ src/ml_exploratory_analysis.py
@@ -4,10 +4,11 @@ from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.feature import HashingTF, Tokenizer, StopWordsRemover
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from bs4 import BeautifulSoup
 
-spark = SparkSession.builder\
-    .appName("ML Analysis")\
-    .master("local[*]")\
+spark = SparkSession.builder \
+    .appName("ML Analysis") \
+    .master("local[*]") \
     .getOrCreate()
 
 schema = StructType([
@@ -21,17 +22,15 @@ schema = StructType([
 
 # Load training data
 tweets_csv = spark \
-    .read.format("csv")\
-    .option("header", "false")\
-    .schema(schema)\
-    .csv("../data/training.1600000.processed.noemoticon.csv") #Sentiment140 dataset
+    .read.format("csv") \
+    .option("header", "false") \
+    .schema(schema) \
+    .csv("../data/training.1600000.processed.noemoticon.csv")
 
 # Trim and clean data of spaces
 reg_exp = "\\s\\s+"
-data = tweets_csv\
-    .select("text", col("target").cast("Int").alias("label"))\
-    .select(trim(col("text")).alias("text"), "label")\
-    .select(regexp_replace(col("text"), reg_exp, " ").alias("text"), "label")
+data = tweets_csv \
+    .select("text", col("target").cast("Int").alias("label"))
 
 # Split the data into train and test
 splits = data.randomSplit([0.6, 0.4], 1234)
@@ -43,9 +42,37 @@ test_rows = test.count()
 print(f"Training data rows: {train_rows}")
 print(f"Testing data rows: {test_rows}")
 
+train.show(10, False)
+
+
+# HTML decoding
+def html_decoding(text):
+    return BeautifulSoup(text, 'html.parser').get_text()
+
+
+udf_html_decoding = udf(html_decoding)
+
+htmlDecodedTrain = train.select(udf_html_decoding(col('text')).alias("text"), "label")
+
+# Remove mentions
+removeMentionsTrain = htmlDecodedTrain.select(regexp_replace(col("text"), "@[A-Za-z0-9_.]+", "").alias("text"), "label")
+
+# Remove URLs
+removeURLTrain = removeMentionsTrain.select(regexp_replace(col("text"), "https?://[A-Za-z0-9./]+", "").alias("text"), "label")
+
+# Remove hashtags, numbers, punctuation
+onlyLettersTrain = removeURLTrain.select(regexp_replace(col("text"), "[^a-zA-Z]", " ").alias("text"), "label")
+
+
+# Trim and clean data of spaces
+cleanTrain = onlyLettersTrain \
+    .select(trim(col("text")).alias("text"), "label") \
+    .select(regexp_replace(col("text"), reg_exp, " ").alias("text"), "label")
+cleanTrain.show(10, False)
+
 # Tokenize data
 tokenizer = Tokenizer(inputCol="text", outputCol="words")
-tokenizedTrain = tokenizer.transform(train)
+tokenizedTrain = tokenizer.transform(cleanTrain)
 tokenizedTrain.show(10, False)
 
 # Removing stop words
@@ -64,7 +91,17 @@ model = nb.fit(numericTrain)
 print("Training is done!")
 
 # Prepare testing data
-tokenizedTest = tokenizer.transform(test)
+cleanTest = test\
+    .select(udf_html_decoding(col('text')).alias("text"), "label") \
+    .select(regexp_replace(col("text"), "@[A-Za-z0-9_.]+", "").alias("text"), "label") \
+    .select(regexp_replace(col("text"), "https?://[A-Za-z0-9./]+", "").alias("text"), "label") \
+    .select(regexp_replace(col("text"), "[^a-zA-Z]", " ").alias("text"), "label") \
+    .select(trim(col("text")).alias("text"), "label") \
+    .select(regexp_replace(col("text"), reg_exp, " ").alias("text"), "label")
+
+cleanTest.show(10, False)
+
+tokenizedTest = tokenizer.transform(cleanTest)
 swrTest = swr.transform(tokenizedTest)
 numericTest = hashTf.transform(swrTest).select(
     'label', 'meaningful_words', 'features'
