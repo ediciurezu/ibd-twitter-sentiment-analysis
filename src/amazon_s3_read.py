bd780a1dd6389dd40731546a86ef094f92b42db0
IoanaMarin1106 <marinvioana@gmail.com>
2022-12-09 23:41:43 +0200

    Minor updates

--- src/amazon_s3_read.py
+++ src/amazon_s3_read.py
@@ -1,7 +1,7 @@
-
 import boto3
 import boto.s3
 from boto.s3.key import Key
+
 
 def read_from_bucket_version_boto3(bucket_name, aws_access_key_id, aws_secret_access_key, key):
     session = boto3.Session(
@@ -11,12 +11,13 @@ def read_from_bucket_version_boto3(bucket_name, aws_access_key_id, aws_secret_ac
 
     s3 = session.resource('s3')
     obj = s3.Object(bucket_name, key)
-    body = obj.get()['Body'].read().decode('utf-8') 
+    body = obj.get()['Body'].read().decode('utf-8')
     print(f'{key} -> {body}')
+
 
 def read_from_bucket_version_boto(bucket_name, aws_access_key_id, aws_secret_access_key, key):
     conn = boto.connect_s3(aws_access_key_id,
-                        aws_secret_access_key)
+                           aws_secret_access_key)
 
     bucket = conn.get_bucket(bucket_name)
     k = Key(bucket)
@@ -25,6 +26,5 @@ def read_from_bucket_version_boto(bucket_name, aws_access_key_id, aws_secret_acc
     print(f'{key} -> {body}')
 
 
-
-read_from_bucket_version_boto3('test-ibd', AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, 'test file ioana')
-read_from_bucket_version_boto('test-ibd', AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, 'test file ioana')
+# read_from_bucket_version_boto3('test-ibd', AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, 'test file ioana')
+# read_from_bucket_version_boto('test-ibd', AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, 'test file ioana')
--- src/tweepy_stream.py
+++ src/tweepy_stream.py
@@ -1,10 +1,11 @@
 import tweepy
 
+
 class TweetPrinter(tweepy.StreamingClient):
     def on_tweet(self, tweet):
         print(tweet)
 
+
 def run_stream(bearer_token):
     printer = TweetPrinter(bearer_token)
     printer.sample()
-
