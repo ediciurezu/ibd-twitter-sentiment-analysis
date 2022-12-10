6fafe27f267efd896fc42135d3ea7142ccf6bdd0
IoanaMarin1106 <marinvioana@gmail.com>
2022-12-10 14:02:26 +0200

    Created web app using Flask - backend done

--- app/amazon_s3_read.py
+++ app/amazon_s3_read.py
@@ -12,7 +12,7 @@ def read_from_bucket_version_boto3(bucket_name, aws_access_key_id, aws_secret_ac
     s3 = session.resource('s3')
     obj = s3.Object(bucket_name, key)
     body = obj.get()['Body'].read().decode('utf-8')
-    print(f'{key} -> {body}')
+    return body
 
 
 def read_from_bucket_version_boto(bucket_name, aws_access_key_id, aws_secret_access_key, key):
---
+++ app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+import boto3
+import json
+
+app = Flask(__name__)
+app.config.from_file("config.json", load=json.load)
+
+# Get config data
+config_data = app.config.get("JSON_AS_ASCII")
+bucket_name = config_data["bucket_name"]
+aws_access_key_id = config_data["aws_access_key_id"]
+aws_secret_access_key = config_data["aws_secret_access_key"]
+
+# Connect to the S3 bucket
+session = boto3.Session(
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+)
+s3 = session.resource('s3')
+
+
+@app.route('/read/<file>', methods=['GET'])
+def read_data(file):
+    # Read the data from the S3 bucket
+    obj = s3.Object(bucket_name, file)
+    body = obj.get()['Body'].read().decode('utf-8')
+    return body
+
+
+if __name__ == '__main__':
+    app.run()
